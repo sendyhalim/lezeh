da5988196fabd7dd3f47c26df49445c58a62e1d3
Sendy Halim <sendyhalim93@gmail.com>
2021-04-22 18:00:21 +0700

    feat(template): Add step to compile custom handlebars template and embed it to the binary

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -1,12 +1,15 @@
-use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
+use std::{borrow::Cow, collections::HashMap};
 
 use serde::Deserialize;
 use serde::Serialize;
 
+use crate::asset::Asset;
 use crate::types::ResultDynError;
 
+/// Phab config
+/// -------------
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct PhabConfig {
   pub host: String,
@@ -15,17 +18,19 @@ pub struct PhabConfig {
   pub pkcs12_password: String,
 }
 
+/// Ghub config
+/// -------------
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GhubConfig {
   pub api_token: String,
 }
 
+/// Deployment config
+/// -------------
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct DeploymentSchemeConfig {
-  pub name: String,
-  pub default_pull_request_title: String,
-  pub merge_from_branch: String,
-  pub merge_into_branch: String,
+pub struct DeploymentConfig {
+  pub repositories: Vec<RepositoryConfig>,
+  pub merge_feature_branches: Option<MergeFeatureBranchesConfig>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -37,10 +42,28 @@ pub struct RepositoryConfig {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct DeploymentConfig {
-  pub repositories: Vec<RepositoryConfig>,
+pub struct DeploymentSchemeConfig {
+  pub name: String,
+  pub default_pull_request_title: String,
+  pub merge_from_branch: String,
+  pub merge_into_branch: String,
 }
 
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct MergeFeatureBranchesConfig {
+  pub output_template_path: Option<String>,
+}
+
+impl Default for MergeFeatureBranchesConfig {
+  fn default() -> Self {
+    return MergeFeatureBranchesConfig {
+      output_template_path: Some("merge_feature_branches_default.hbs".to_owned()),
+    };
+  }
+}
+
+/// Bitly config
+/// -------------
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct BitlyConfig {
   pub api_token: String,
@@ -57,7 +80,11 @@ pub struct Config {
 impl Config {
   pub fn new(setting_path: impl AsRef<Path>) -> ResultDynError<Config> {
     let config_str = fs::read_to_string(setting_path)?;
-    let config: Config = serde_yaml::from_str(&config_str)?;
+    let mut config: Config = serde_yaml::from_str(&config_str)?;
+
+    if config.deployment.merge_feature_branches.is_none() {
+      config.deployment.merge_feature_branches = Some(Default::default());
+    }
 
     return Ok(config);
   }
--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,6 +1,8 @@
+pub mod asset;
 pub mod clients;
 pub mod command;
 pub mod config;
 pub mod io_logger;
+pub mod renderers;
 pub mod types;
 pub mod utils;
