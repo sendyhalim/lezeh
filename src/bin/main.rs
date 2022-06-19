d8e87db60afc5a5be565a2a9b7b5e6bf1adfa9a5
Sendy Halim <sendyhalim93@gmail.com>
2022-06-19 23:15:56 +0700

    Move cherry pick logic into db/cli

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,7 +4,6 @@ use std::collections::HashSet;
 use clap::App as Cli;
 use clap::Arg;
 use clap::ArgMatches;
-use clap::SubCommand;
 
 use lib::common::rose_tree::RoseTreeNode;
 use lib::db::psql;
@@ -25,48 +24,6 @@ pub mod built_info {
 #[tokio::main]
 async fn main() -> ResultAnyError<()> {
   env_logger::init();
-
-  let handle = std::thread::spawn(|| {
-    let psql = psql::connection::PsqlConnection::new(&lib::db::psql::connection::PsqlCreds {
-      host: "localhost".to_owned(),
-      database_name: "gepeel_app".to_owned(),
-      username: "sendyhalim".to_owned(),
-      password: Option::None,
-    })
-    .unwrap();
-
-    let mut relation_fetcher = psql::relation_fetcher::RelationFetcher::new(psql);
-
-    let psql_table_by_name = relation_fetcher.load_table_structure(Option::None).unwrap();
-
-    let input = psql::relation_fetcher::FetchRowInput {
-      schema: Some("public"),
-      table_name: "store_staffs",
-      column_name: "email",
-      column_value: Box::new("bigpawofficial@gmail.com"),
-    };
-
-    let mut trees = relation_fetcher
-      .fetch_rose_trees_to_be_inserted(&input, &psql_table_by_name)
-      .unwrap();
-
-    let tree: RoseTreeNode<PsqlTableRows> = trees.remove(0);
-    println!("tree {:#?}", tree);
-
-    let mut parents_by_level: HashMap<i32, HashSet<_>> =
-      RoseTreeNode::parents_by_level(tree.clone());
-    let children_by_level: HashMap<i32, HashSet<_>> =
-      RoseTreeNode::children_by_level(tree, &mut parents_by_level);
-
-    println!("{:#?}", parents_by_level);
-    println!("{:#?}", children_by_level);
-    let statements: Vec<String> =
-      lib::db::psql::relation_insert::RelationInsert::into_insert_statements(parents_by_level);
-
-    println!("{:#?}", statements);
-  });
-
-  handle.join().unwrap();
 
   let log_decorator = slog_term::TermDecorator::new().build();
   let log_drain = slog_term::CompactFormat::new(log_decorator).build().fuse();
@@ -96,7 +53,7 @@ async fn main() -> ResultAnyError<()> {
   match cli.subcommand() {
     ("deployment", Some(cli)) => DeploymentCli::run(cli, config, logger).await?,
     ("url", Some(url_cli)) => UrlCli::run(url_cli, config).await?,
-    ("db", Some(db_cli)) => DbCli::run(db_cli, config).await?,
+    ("db", Some(db_cli)) => DbCli::run(db_cli, config, logger).await?,
     _ => {}
   }
 
--- src/lib/db/cli.rs
+++ src/lib/db/cli.rs
@@ -1,10 +1,18 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+use anyhow::anyhow;
 use clap::App as Cli;
 use clap::Arg;
 use clap::ArgMatches;
 use clap::SubCommand;
+use slog::Logger;
 
 use crate::common::config::Config;
+use crate::common::config::DbConfig;
+use crate::common::rose_tree::RoseTreeNode;
 use crate::common::types::ResultAnyError;
+use crate::db::psql;
 
 pub struct DbCli {}
 
@@ -17,25 +25,37 @@ impl DbCli {
         SubCommand::with_name("cherry-pick")
           .about("Cherry pick data from the given db source into the db target")
           .arg(
+            Arg::with_name("schema")
+              .long("--schema")
+              .required(false)
+              .help("Db schema"),
+          )
+          .arg(
             Arg::with_name("table")
               .long("--table")
               .required(true)
               .help("Db table"),
           )
           .arg(
-            Arg::with_name("ids")
-              .long("--ids")
-              .required(true)
-              .help("Comma separated ids"),
+            Arg::with_name("column")
+              .long("--column")
+              .required(false)
+              .help("The column that the values are tied to, default to id"),
           )
           .arg(
-            Arg::with_name("source-db")
+            Arg::with_name("values")
+              .long("--values")
+              .required(true)
+              .help("Comma separated values of the column to be fetched"),
+          )
+          .arg(
+            Arg::with_name("source_db")
               .long("--source-db")
               .required(true)
               .help("Source db to fetch data from"),
           )
           .arg(
-            Arg::with_name("target-db")
+            Arg::with_name("target_db")
               .required(true)
               .long("--target-db")
               .help("Target db to insert db"),
@@ -43,7 +63,104 @@ impl DbCli {
       );
   }
 
-  pub async fn run(cli: &ArgMatches<'_>, config: Config) -> ResultAnyError<()> {
+  pub async fn run(cli: &ArgMatches<'_>, config: Config, logger: Logger) -> ResultAnyError<()> {
+    match cli.subcommand() {
+      ("cherry-pick", Some(cherry_pick_cli)) => DbCli::cherry_pick(
+        cherry_pick_cli.value_of("source_db").map(|s| s.to_owned()),
+        cherry_pick_cli.value_of("target_db").map(|s| s.to_owned()),
+        cherry_pick_cli.value_of("schema").map(|s| s.to_owned()),
+        cherry_pick_cli.value_of("table").map(|s| s.to_owned()),
+        cherry_pick_cli.value_of("column").map(|s| s.to_owned()),
+        cherry_pick_cli.value_of("values").map(|s| s.to_owned()),
+        config,
+        logger,
+      ),
+      _ => Ok(()),
+    }
+  }
+
+  /// TODO:
+  /// * Still broken, cannot find source DB input
+  /// * Refactor multiple .map on clis
+  /// * Can we not spawn a new thread to just run it?
+  /// * Need to apply the inserts on target db
+  pub fn cherry_pick<'a>(
+    source_db: Option<String>,
+    target_db: Option<String>,
+    schema: Option<String>,
+    table: Option<String>,
+    column: Option<String>,
+    values: Option<String>,
+    config: Config,
+    logger: Logger,
+  ) -> ResultAnyError<()> {
+    let source_db: String = source_db.unwrap();
+    let target_db: String = target_db.unwrap();
+    let schema: String = schema.or(Some("public".to_owned())).unwrap();
+    let table: String = table.unwrap();
+    let column: String = column.or(Some("id".to_owned())).unwrap();
+    let values: Vec<String> = values
+      .unwrap()
+      .split(',')
+      .map(|val| val.trim().to_owned())
+      .collect();
+
+    let db_by_name: HashMap<String, DbConfig> = config
+      .db_by_name
+      .ok_or_else(|| anyhow!("Db config is not set"))?;
+
+    let source_db_config: DbConfig = db_by_name
+      .get(&source_db)
+      .ok_or_else(|| anyhow!("Source db {} is not registered", source_db))?
+      .clone();
+
+    let target_db_config: DbConfig = db_by_name
+      .get(&target_db)
+      .ok_or_else(|| anyhow!("Target db {} is not registered", target_db))?
+      .clone();
+
+    let handle = std::thread::spawn(move || {
+      let psql = psql::connection::PsqlConnection::new(&psql::connection::PsqlCreds {
+        host: source_db_config.host.clone(),
+        database_name: source_db_config.database.clone(),
+        username: source_db_config.username.clone(),
+        password: source_db_config.password.clone(),
+      })
+      .unwrap();
+
+      let mut relation_fetcher = psql::relation_fetcher::RelationFetcher::new(psql);
+
+      let psql_table_by_name = relation_fetcher.load_table_structure(Option::None).unwrap();
+
+      let input = psql::relation_fetcher::FetchRowInput {
+        schema: Some(&schema),
+        table_name: &table,
+        column_name: &column,
+        column_value: Box::new(values.get(0).unwrap().clone()), // As of now only supports 1 value
+      };
+
+      let mut trees = relation_fetcher
+        .fetch_rose_trees_to_be_inserted(&input, &psql_table_by_name)
+        .unwrap();
+
+      let tree: RoseTreeNode<psql::dto::PsqlTableRows> = trees.remove(0);
+      println!("tree {:#?}", tree);
+
+      let mut parents_by_level: HashMap<i32, HashSet<_>> =
+        RoseTreeNode::parents_by_level(tree.clone());
+      let children_by_level: HashMap<i32, HashSet<_>> =
+        RoseTreeNode::children_by_level(tree, &mut parents_by_level);
+
+      println!("{:#?}", parents_by_level);
+      println!("{:#?}", children_by_level);
+      let statements: Vec<String> =
+        psql::relation_insert::RelationInsert::into_insert_statements(parents_by_level);
+
+      println!("{:#?}", statements);
+    });
+
+    handle.join().unwrap();
+
     return Ok(());
   }
 }
